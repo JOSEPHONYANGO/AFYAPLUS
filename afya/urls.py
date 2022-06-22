3799734ac6cfe9cc53e066d5fe276966cdadb843
JOSEPHONYANGO <joseph.onyango@student.moringaschool.com>
2022-06-22 22:06:31 +0300

    templates

--- afya/urls.py
+++ afya/urls.py
@@ -2,12 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.is_doctor,name='doctor'),
-    path('auth/login/', views.login_user,name='login'),
-    path('accounts/logout/', views.logout_user, name='logout'),
-    path('auth/register/', views.register_user,name='register'),
-    path('profile/', views.profile,name='profile'),
-    path('project/<project_id>', views.project,name='project'),
-    path('api/profiles/', views.ProfileList.as_view()),
-    path('api/projects/', views.ProjectsList.as_view())
+    # path('',views.is_doctor, name='doctor'),
+    path('', views.doctor_dashboard_view,name='dashboard'),
+    # path('accounts/logout/', views.logout_user, name='logout'),
+    # path('auth/register/', views.register_user,name='register'),
+    # path('profile/', views.profile,name='profile'),
+    # path('project/<project_id>', views.project,name='project'),
+    # path('api/profiles/', views.ProfileList.as_view()),
+    # path('api/projects/', views.ProjectsList.as_view())
 ]
