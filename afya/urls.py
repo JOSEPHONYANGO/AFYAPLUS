29ae431ba4178bd1de1e06a9de6eb090ebee3d48
JOSEPHONYANGO <joseph.onyango@student.moringaschool.com>
2022-06-23 11:14:39 +0300

    final

--- afya/urls.py
+++ afya/urls.py
@@ -3,11 +3,13 @@ from . import views
 
 urlpatterns = [
     path('',views.home_view, name='index'),
-    path('hospital/doctor_dashboard/', views.doctor_dashboard_view,name='doctor_dashboard'),
-    path('hospital/doctor_patient/', views.doctor_patient_view, name='doctor_patient'),
-    # path('auth/register/', views.register_user,name='register'),
-    # path('profile/', views.profile,name='profile'),
-    # path('project/<project_id>', views.project,name='project'),
-    # path('api/profiles/', views.ProfileList.as_view()),
+    path('hospital/doctor_dashboard/', views.doctor_dashboard_view,name='hospital/doctor_dashboard.html'),
+    path('hospital/doctor_patient/', views.doctor_patient_view, name='hospital/doctor_patient'),
+    path('hospital/doctor_view_patient', views.doctor_view_patient_view, name='hospital/doctor_view_patient'),
+    path('doctor_view_discharge_patient/', views.doctor_view_discharge_patient_view, name='hospital/doctor_view_discharge_patient'),
+    path('hospital/doctor_appointment/', views.doctor_appointment_view, name='hospital/doctor_appointment'),
+    path('hospital/doctor_view_appointment/', views.doctor_view_appointment_view, name='hospital/doctor_view_appointment'),
+    path('hospital/doctor_delete_appointment/', views.doctor_delete_appointment_view, name='hospital/doctor_delete_appointment/'),
+    
     # path('api/projects/', views.ProjectsList.as_view())
 ]
