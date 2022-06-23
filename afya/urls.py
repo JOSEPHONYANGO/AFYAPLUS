cefd86eb2ee5df1c196d9a07906f8946c8831d49
JOSEPHONYANGO <joseph.onyango@student.moringaschool.com>
2022-06-23 09:10:32 +0300

    cards

--- afya/urls.py
+++ afya/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # path('',views.is_doctor, name='doctor'),
-    path('', views.doctor_dashboard_view,name='dashboard'),
-    # path('accounts/logout/', views.logout_user, name='logout'),
+    path('',views.home_view, name='index'),
+    path('hospital/doctor_dashboard/', views.doctor_dashboard_view,name='doctor_dashboard'),
+    path('hospital/doctor_patient/', views.doctor_patient_view, name='doctor_patient'),
     # path('auth/register/', views.register_user,name='register'),
     # path('profile/', views.profile,name='profile'),
     # path('project/<project_id>', views.project,name='project'),
